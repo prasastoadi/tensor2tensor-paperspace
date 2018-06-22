3272226a786076e32d0d583ff5c1db4789527e7f
Prasasto Adi <prasastoadi@gmail.com>
2018-06-22 22:34:37 +0700

    BSxxxx_Max256_LR.2_LRWS16000_single
    BS2048_Max256_LR.2_LRWS16000_single
    BS4096_Max256_LR.2_LRWS16000_single

---
+++ BS2048_Max256_LR.2_LRWS16000_single.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pip install tensor2tensor
+
+t2t-trainer \
+--problem=translate_enid_iwslt32k \
+--model=transformer \
+--hparams_set=transformer_base \
+--data_dir=/storage/data/ \
+--output_dir=/storage/train/BS2048_Max256_LR.2_LRWS16000_single/ \
+--eval_steps=100 \
+--keep_checkpoint_max=100 \
+--local_eval_frequency=2500 \
+--train_steps=50000 \
+--hparams=max_length=256,batch_size=2048,learning_rate=0.2,learning_rate_warmup_steps=16000 \
+--generate_data
--- BS4096_Max256_LR.2_LRWS16000_single.sh
+++ BS4096_Max256_LR.2_LRWS16000_single.sh
@@ -6,10 +6,10 @@ t2t-trainer \
 --model=transformer \
 --hparams_set=transformer_base \
 --data_dir=/storage/data/ \
---output_dir=/storage/train/BS1024_Max256_LR.2_LRWS16000_test \
+--output_dir=/storage/train/BS4096_Max256_LR.2_LRWS16000_single/ \
 --eval_steps=100 \
---keep_checkpoint_max=50 \
+--keep_checkpoint_max=100 \
 --local_eval_frequency=2500 \
 --train_steps=50000 \
---hparams=max_length=256,batch_size=1024,learning_rate=0.2,learning_rate_warmup_steps=16000 \
+--hparams=max_length=256,batch_size=4096,learning_rate=0.2,learning_rate_warmup_steps=16000 \
 --generate_data
