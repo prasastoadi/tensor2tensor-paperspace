535714d12a6cb80d1cd76fec9f42901f4e47bf06
Prasasto Adi <prasastoadi@gmail.com>
2018-08-05 23:09:23 +0700

    Add nothrott

---
+++ BS4096_Max256_LR.2_LRWS8000_single_notok_nothrott.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pip install tensor2tensor
+
+t2t-trainer \
+--problem=translate_enid_iwslt32k_notok \
+--model=transformer \
+--hparams_set=transformer_base \
+--data_dir=/storage/data_notok/ \
+--output_dir=/storage/train/BS4096_Max256_LR.2_LRWS8000_single_notok_nothrott/ \
+--eval_steps=100 \
+--keep_checkpoint_max=100 \
+--local_eval_frequency=5000 \
+--eval_throttle_seconds=60000 \
+--train_steps=100000 \
+--hparams=max_length=256,batch_size=4096,learning_rate=0.2,learning_rate_warmup_steps=8000 \
+--generate_data \
+--t2t_usr_dir=usrdir
