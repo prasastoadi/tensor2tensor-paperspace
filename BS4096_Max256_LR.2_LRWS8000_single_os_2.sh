7604cef05335ff9d8352e1897f474829d8350ae4
Prasasto Adi <prasastoadi@gmail.com>
2018-08-14 17:05:46 +0700

    Add apropriate setting for OpenSubtitle

---
+++ BS4096_Max256_LR.2_LRWS8000_single_os_2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install tensor2tensor==1.7.0
+
+t2t-trainer \
+--problem=translate_enid_iwslt32k_os \
+--model=transformer \
+--hparams_set=transformer_base \
+--data_dir=/storage/data_os/ \
+--output_dir=/storage/train/BS4096_Max256_LR.2_LRWS8000_single_os_2/ \
+--eval_steps=100 \
+--keep_checkpoint_max=100 \
+--local_eval_frequency=5000 \
+--eval_throttle_seconds=60000 \
+--train_steps=100000 \
+--hparams=max_length=256,batch_size=4096,learning_rate=0.2,learning_rate_warmup_steps=8000 \
+--generate_data \
+--t2t_usr_dir=usrdir
+
+t2t-translate-all \
+--t2t_usr_dir=usrdir \
+--problem=translate_enid_iwslt32k_os \
+--model_dir=/storage/train/BS4096_Max256_LR.2_LRWS8000_single_os_2 \
+--source=/storage/corpus/dev/IWSLT17.TED.dev2010.en-id/IWSLT17.TED.dev2010.en-id.en \
+--translations_dir=/storage/translated/dev/BS4096_Max256_LR.2_LRWS8000_single_os_2/IWSLT17.TED.dev2010.en-id \
+--beam_size=4 \
+--alpha=0.6 \
+--model=transformer \
+--data_dir=/storage/data_os \
+--hparams_set=transformer_base \
+--hparams=max_length=256,batch_size=4096,learning_rate=0.2,learning_rate_warmup_steps=8000 \
+--min_steps=5000
+
+t2t-bleu \
+--reference=/storage/corpus/dev/IWSLT17.TED.dev2010.en-id/IWSLT17.TED.dev2010.en-id.id \
+--translations_dir=/storage/translated/dev/BS4096_Max256_LR.2_LRWS8000_single_os_2/IWSLT17.TED.dev2010.en-id \
+--event_dir=/storage/translated/dev/BS4096_Max256_LR.2_LRWS8000_single_os_2/IWSLT17.TED.dev2010.en-id
