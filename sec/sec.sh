1a3880bb82496962d00883b88bc8bda1ac654b52
Prasasto Adi <prasastoadi@gmail.com>
2018-09-28 06:40:41 +0700

    sec v100

--- sec/sec.sh
+++ sec/sec.sh
@@ -6,12 +6,11 @@ t2t-trainer \
 --model=transformer \
 --hparams_set=transformer_base \
 --data_dir=/storage/data_notok_plus_os/ \
---output_dir=/storage/train/BS4096_Max128_LR.2_LRWS8000_base_notok_plus_os/ \
+--output_dir=/storage/train/BS4096_Max128_LR.2_LRWS8000_base_notok_plus_os_v100/ \
 --eval_steps=100 \
 --keep_checkpoint_max=100 \
 --local_eval_frequency=5000 \
 --eval_throttle_seconds=600000 \
 --train_steps=5000 \
 --hparams=max_length=256,batch_size=4096,learning_rate=0.2,learning_rate_warmup_steps=8000 \
---generate_data \
 --t2t_usr_dir=usrdir
